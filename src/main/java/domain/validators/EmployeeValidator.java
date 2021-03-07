0283f50ac20335a44288b5058829f37e6c3a5a14
Brasovean Dan <61885079+brasoveandan@users.noreply.github.com>
2021-03-07 22:38:27 +0200

    Update Employee Entity

--- src/main/java/domain/validators/EmployeeValidator.java
+++ src/main/java/domain/validators/EmployeeValidator.java
@@ -2,7 +2,22 @@ package domain.validators;
 
 import domain.Employee;
 
+import java.util.regex.Pattern;
+
 public class EmployeeValidator implements Validator<Employee> {
+    public static boolean mailValidation(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+
     @Override
     public void validate(Employee employee) throws ValidationException {
         String message="";
@@ -17,6 +32,12 @@ public class EmployeeValidator implements Validator<Employee> {
         }
         if (employee.getLastName() == null || employee.getLastName().equals("")) {
             message += "Numele nu poate fi vid.\n";
+        }
+        if (mailValidation(employee.getMail())) {
+            message += "E-mail invalid.\n";
+        }
+        if (employee.getPhoneNumber() == null || employee.getPhoneNumber().length() != 10) {
+            message += "Numar de telefon invalid.\n";
         }
         if (employee.getSocialSecurityNumber() == null || employee.getSocialSecurityNumber().length()!=13) {
             message += "CNP-ul trebuie sa contina 13 cifre.\n";
